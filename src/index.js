a3f9b2cd5282456f638521583dc445cbe017798e
darryladr <darryl.adriansyah@binus.ac.id>
2022-07-15 16:54:34 +0700

    [WIP] Migrate to SCSS and use jQuery

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+require("@babel/core").transform("code", {
+    presets: ["@babel/preset-env"],
+});
+
 import './sass/main.scss'
+import $ from 'jquery-ui'
+
+$(() => {
+    $("button").on("click", () => {
+        $("button").toggleClass("active")
+    })
+})
